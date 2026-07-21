80eb53eda7cc93b3295696b161e51b86fca64af2
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-21 20:20:51 +0300

    feat: experimenting with zustand

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,71 +1,32 @@
 "use client";
 
-import { useState } from "react";
+import { create } from "zustand";
 
-const categories: string[] = ["Flat", "House", "Duplex"];
-
-interface Listing {
-  category: string;
+interface Cart {
   name: string;
-  description?: string;
+  description: string;
+  price: number;
 }
 
-const listings: Listing[] = [
-  {
-    category: "Flat",
-    name: "A nice flat near downtown",
-    description:
-      "For God so loved the world that He gave His only begotten Son that whoever believes in Him shall not perish but have eternal life.",
-  },
-  {
-    category: "Duplex",
-    name: "A clean duplex near the highway",
-    description: "For whoever calls on the Name of the Lord will be saved.",
-  },
-  {
-    category: "House",
-    name: "A good house in a sleepy neighborhood",
-    description:
-      "Truly truly I say to you: he who believes in Me has eternal life.",
-  },
-];
+interface IContext {
+  cart: Cart[];
+}
 
-export default function Page() {
-  const [selectedCategory, setSelectedCategory] = useState<string>("");
+const useContext = create<IContext>((set) => ({
+  cart: [],
+}));
+
+export default function IndexPage() {
+  const cart = useContext((state) => state.cart);
 
   return (
-    <div className="p-3 mx-auto max-w-md w-full flex flex-col gap-3">
-      <div className="flex gap-1">
-        <button
-          className="px-2 rounded-md bg-stone-200 hover:bg-stone-300 duration-300 cursor-pointer"
-          onClick={() => setSelectedCategory("")}
-        >
-          None
-        </button>
-        {categories.map((category, index) => (
-          <button
-            className={`${selectedCategory === category ? "bg-stone-300" : "bg-stone-200"} px-2 rounded-md hover:bg-stone-300 duration-300 cursor-pointer`}
-            key={index}
-            onClick={() => setSelectedCategory(category)}
-          >
-            {category}
-          </button>
-        ))}
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
-        {(selectedCategory
-          ? listings.filter((listing) => listing.category === selectedCategory)
-          : listings
-        ).map((listing, index) => (
-          <div
-            className="rounded-md border flex flex-col w-full p-3 gap-3"
-            key={index}
-          >
-            <h2 className="text-lg">{listing.name}</h2>
-            <p>{listing.description}</p>
-            <span className="italic">{listing.category}</span>
+    <div className="bg-linear-to-br to-sky-50 min-h-screen">
+      <div className="p-3 max-w-md mx-auto w-full">
+        <form className="bg-white rounded-md p-3">
+          <div className="flex flex-col gap-1">
+            <label htmlFor=""></label>
           </div>
-        ))}
+        </form>
       </div>
     </div>
   );
