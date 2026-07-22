5c147ecbb8b0e3a4f77bd0ba170cc6e4342af348
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-22 09:53:13 +0300

    chore: trying to make a correct reference

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+
+import { useEffect } from "react";
+
+const BiblePlaces = ["2 Corinthians 5:17"];
+
+const apiUrl = "https://open-bible-api.vercel.app";
+
+const fetchVerse = async (BiblePlace: string) => {
+  const [BookAndChapter, verseNumber] = BiblePlace.split(":");
+  for (let i = BookAndChapter.length; i > 0; i--) {
+    const place = BookAndChapter[i];
+    console.log(place);
+  }
+
+  // const response = await fetch(`${apiUrl}/`);
+};
+
 export default function IndexPage() {
-  return "Jesus is LORD";
+  useEffect(() => {
+    for (const place of BiblePlaces) {
+      fetchVerse(place);
+    }
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-sky-50">
+      <div className="p-3 max-w-md mx-auto w-full"></div>
+    </div>
+  );
 }
