8e0e0b1639874b3c1b9dae436a4410c10865d0c6
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-22 10:17:44 +0300

    revert: repent of the changes, thought it was playground

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,40 +1,72 @@
 "use client";
 
-import { useEffect } from "react";
+import { useState } from "react";
 
-const BiblePlaces = ["2 Corinthians 5:17", "Romans 8:13", "Romans 12:14"];
+const categories: string[] = ["Flat", "House", "Duplex"];
 
-const apiUrl = "https://open-bible-api.vercel.app";
+interface Listing {
+  category: string;
+  name: string;
+  description?: string;
+}
 
-const reverse = (data: string) => {
-  return data.split("").reverse().join("");
-};
+const listings: Listing[] = [
+  {
+    category: "Flat",
+    name: "A nice flat near downtown",
+    description:
+      "For God so loved the world that He gave His only begotten Son that whoever believes in Him shall not perish but have eternal life.",
+  },
+  {
+    category: "Duplex",
+    name: "A clean duplex near the highway",
+    description: "For whoever calls on the Name of the Lord will be saved.",
+  },
+  {
+    category: "House",
+    name: "A good house in a sleepy neighborhood",
+    description:
+      "Truly truly I say to you: he who believes in Me has eternal life.",
+  },
+];
 
-const fetchVerse = async (BiblePlace: string) => {
-  const [BookAndChapter, verseNumber] = BiblePlace.split(":");
-  let chapter: string = "";
-  for (let i = BookAndChapter.length - 1; i > 0; i--) {
-    const place = BookAndChapter[i];
-    if (place === " ") break;
-    chapter += place;
-  }
-  chapter = reverse(chapter);
-  const chapterNumber: number = Number.parseInt(chapter);
-  console.log(chapterNumber);
-
-  // const response = await fetch(`${apiUrl}/`);
-};
-
-export default function IndexPage() {
-  useEffect(() => {
-    for (const place of BiblePlaces) {
-      fetchVerse(place);
-    }
-  }, []);
+export default function Page() {
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
 
   return (
-    <div className="min-h-screen bg-sky-50">
-      <div className="p-3 max-w-md mx-auto w-full"></div>
+    <div className="p-3 mx-auto max-w-md w-full flex flex-col gap-3">
+      <div className="flex gap-1">
+        <button
+          className="px-2 rounded-md bg-stone-200 hover:bg-stone-300 duration-300 cursor-pointer"
+          onClick={() => setSelectedCategory("")}
+        >
+          None
+        </button>
+        {categories.map((category, index) => (
+          <button
+            className={`${selectedCategory === category ? "bg-stone-300" : "bg-stone-200"} px-2 rounded-md hover:bg-stone-300 duration-300 cursor-pointer`}
+            key={index}
+            onClick={() => setSelectedCategory(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
+        {(selectedCategory
+          ? listings.filter((listing) => listing.category === selectedCategory)
+          : listings
+        ).map((listing, index) => (
+          <div
+            className="rounded-md border flex flex-col w-full p-3 gap-3"
+            key={index}
+          >
+            <h2 className="text-lg">{listing.name}</h2>
+            <p>{listing.description}</p>
+            <span className="italic">{listing.category}</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
