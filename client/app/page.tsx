6a8a640563c25a316784928f6ec122fb786eb7bb
Oleg Onyshchenko <seesmwork@gmail.com>
2026-07-22 09:59:49 +0300

    feat: get a correct chapter number

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,15 +6,21 @@ const BiblePlaces = ["2 Corinthians 5:17", "Romans 8:13", "Romans 12:14"];
 
 const apiUrl = "https://open-bible-api.vercel.app";
 
+const reverse = (data: string) => {
+  return data.split("").reverse().join("");
+};
+
 const fetchVerse = async (BiblePlace: string) => {
   const [BookAndChapter, verseNumber] = BiblePlace.split(":");
-  let chapter = "";
+  let chapter: string = "";
   for (let i = BookAndChapter.length - 1; i > 0; i--) {
     const place = BookAndChapter[i];
     if (place === " ") break;
     chapter += place;
   }
-  console.log(chapter);
+  chapter = reverse(chapter);
+  const chapterNumber: number = Number.parseInt(chapter);
+  console.log(chapterNumber);
 
   // const response = await fetch(`${apiUrl}/`);
 };
